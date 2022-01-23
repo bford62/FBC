aa79bd3c3958995000e12b28a57115daeae078ae
bob.ford@wowinc.com <bford@wideopenwest.com>
2022-01-23 15:47:45 -0600

    Gatsby Tutorial Part 2 COmpleted

---
+++ src/pages/WhoWeAre.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const WhoWeAre = () => {
+  return (
+    <Layout pageTitle>Who We Are>
+      <h2>Who We Are</h2>
+      <p>Friendship Baptist Church exists to transform the spiritual landscape of western Will County by loving God, by making friends with people who don’t know God yet, and by helping people to become fully-devoted followers of Jesus Christ.  We believe the Great Commandment (<a href="https://www.biblegateway.com/passage/?search=Matthew+22%3A37-40&version=NIV">Matthew 22:37-40</a>) and the Great Commission (<a href="https://www.biblegateway.com/passage/?search=Matthew+28%3A18-20&version=NIV">Matthew 28:18-20</a>) give us the five biblical purposes of the church that must drive us.  Those purposes follow:</p>
+      <ol>
+        <li>Worship -- We will love God with all our heart.  We will participate in public worship services together and maintain personal daily devotions. (<a href="https://www.biblegateway.com/passage/?search=John+4%3A24&version=NIV">John 4:24</a>; <a href="https://www.biblegateway.com/passage/?search=Hebrews+10%3A25&version=NIV">Hebrews 10:25</a>)</li>
+        <li>Evangelism/Missions -- We will go and make disciples.  We will share the good news of Jesus Christ with as many people as possible in our community and throughout the world. (<a href="https://www.biblegateway.com/passage/?search=Matthew+28%3A18-20&version=NIV">Matthew 28:18-20</a>; <a href="https://www.biblegateway.com/passage/?search=Acts+1%3A8&version=NIV">Acts 1:8</a>; <a href="https://www.biblegateway.com/passage/?search=2+Peter+3%3A9&version=NIV">2 Peter 3:9</a>)</li>
+        <li>Fellowship -- We will baptize people, bringing them into God’s family.  We will encourage, support and pray for each other as members of the family of God, sharing our lives together. (<a href="https://www.biblegateway.com/passage/?search=1+John+1%3A7&version=NIV">1 John 1:7</a>; <a href="https://www.biblegateway.com/passage/?search=Acts+2%3A42-47&version=NIV">Acts 2:42-47</a>; <a href="https://www.biblegateway.com/passage/?search=Hebrews+10%3A23-25&version=NIV">Hebrews 10:23-25</a>; <a href="">Romans 15:5-7</a>, <a href="https://www.biblegateway.com/passage/?search=John+13%3A34-35&version=NIV">John 13:34-35</a>)</li>
+        <li>Discipleship -- We will teach people to obey all of God’s instructions.  We will help members develop toward full Christian maturity and train them for effective ministry.  We will promote personal spiritual growth and discipleship through Bible teaching. (<a href="https://www.biblegateway.com/passage/?search=Ephesians+4%3A11-13&version=NIV">Ephesians 4:11-13</a>; <a href="https://www.biblegateway.com/passage/?search=2+Timothy+2%3A2&version=NIV">2 Timothy 2:2</a>)</li>
+        <li>Ministry/Service -- We will love our neighbors as ourselves.  We will serve unselfishly, in Jesus’ name meeting the physical, emotional, and spiritual needs of those in our church, community and world. (<a href="https://www.biblegateway.com/passage/?search=1+Peter+4%3A10-11&version=NIV">1 Peter 4:10-11</a>; <a href="https://www.biblegateway.com/passage/?search=Matthew+25%3A34-40&version=NIV">Matthew 25:34-40</a>; <a href="https://www.biblegateway.com/passage/?search=1+Thessalonians+5%3A11&version=NIV">1 Thessalonians 5:11</a>, <a href="https://www.biblegateway.com/passage/?search=Galatians+5%3A13&version=NIV">Galatians 5:13</a>)</li>
+      </ol>
+      <p>In living out these purposes, Friendship Baptist Church seeks to benefit the people of the Plainfield area by providing opportunities for spiritual, physical, intellectual, social and cultural development (<a href="https://www.biblegateway.com/passage/?search=Luke+2%3A52&version=NIV">Luke 2:52</a>).</p>
+
+
+
+      <p>Friendship Baptist Church Is a congregation of believers centered around the mission of:  Love God, Love People, Light Your World!  Our desire is to personally grow and mature in our knowledge and love for the Lord, to actively demonstrate love and support to help each other grow in faith and maturity, and to actively and intentionally build relationships with others in our neighborhoods and communities in order to demonstrate Christ’s love for them and to share the good news of the gospel.</p>
+      <p>Friendship Baptist is a mature established church located on 3 acres of land in Plainfield IL, in the heart of the Southwestern Chicago suburbs.</p>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default WhoWeAre
